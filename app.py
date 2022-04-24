709d1a10c0a3d3a9e6a916da57a72229f24e04a3
shaheer-2000 <shaheer.2000@hotmail.com>
2022-04-24 23:11:32 +0500

    Another one

--- app.py
+++ app.py
@@ -6,11 +6,8 @@ from index import init
 load_dotenv()
 app = Flask(__name__)
 
-q = None
-
 @app.route("/", methods=["GET", "POST"])
 def get_docs():
-	global q
 	if "query" not in request.json:
 		return { "error": "ERROR!" }
 	return jsonify(q.process(request.json["query"], alpha=float(environ.get("THRESHOLD_VALUE"))))
