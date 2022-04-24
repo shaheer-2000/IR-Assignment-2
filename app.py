a2f0c4fc25f99e9e917cc064f55d2ff7c071b593
shaheer-2000 <shaheer.2000@hotmail.com>
2022-04-24 23:13:39 +0500

    Another one

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ from index import init
 load_dotenv()
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def get_docs():
-	if "query" not in request.json:
-		return { "error": "ERROR!" }
-	return jsonify(q.process(request.json["query"], alpha=float(environ.get("THRESHOLD_VALUE"))))
-	# return { "ok": "OK!" }
-
 if __name__ == '__main__':
 	q = init()
+
+	@app.route("/", methods=["GET", "POST"])
+	def get_docs():
+		if "query" not in request.json:
+			return { "error": "ERROR!" }
+		return jsonify(q.process(request.json["query"], alpha=float(environ.get("THRESHOLD_VALUE"))))
+		# return { "ok": "OK!" }
+
+
 	app.run(port=5000)
 	
