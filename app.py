bdc374e182511ea249152e27eb29398b335a63b7
shaheer-2000 <shaheer.2000@hotmail.com>
2022-04-24 23:03:19 +0500

    Test

--- app.py
+++ app.py
@@ -1,64 +1,18 @@
-from distutils.log import debug
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from os import environ
-from lib.preprocessor import Preprocessor
-from lib.reader import Reader
-from lib.index_builder import IndexBuilder
-from lib.query_processor import QueryProcessor
-import pickle
+# from index import q
 
-from flask import Flask, request, jsonify
+load_dotenv()
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def get_docs():
+	if "query" not in request.json:
+		return { "error": "ERROR!" }
+	# return jsonify(q.process(request.json["query"], alpha=float(environ.get("THRESHOLD_VALUE"))))
+	return { "ok": "OK!" }
 
 if __name__ == '__main__':
-	load_dotenv()
-
-	r = Reader(collection_path=environ.get("COLLECTION_PATH"))
-	stopwords = list(filter(lambda s: s != " ", r.read_file(environ.get("STOPWORDS_PATH")).splitlines()))
-	p = Preprocessor(stopwords=stopwords)
-
-	r.mkdir(environ.get("DUMPS_PATH"))
-
-	preprocessed_docs = None
-
-	if not r.path_exists(environ.get("PREPROCESSED_DUMP")):
-		docs = r.get_documents()
-		abstract = r.read_file(docs[0])
-
-		abstracts = []
-		for doc in docs:
-			abstracts.append(r.read_file(doc))
-
-		preprocessed_docs = p.preprocess_pipe(abstracts)
-	
-		with open(r.resolve_path(environ.get("PREPROCESSED_DUMP")), "wb") as output:
-			pickle.dump(preprocessed_docs, output)
-	else:
-		with open(r.resolve_path(environ.get("PREPROCESSED_DUMP")), "rb") as input:
-			preprocessed_docs = pickle.load(input)
-
-	index = None
-	if not r.path_exists(environ.get("INDEX_DUMP")):
-		i = IndexBuilder()
-		i.process(preprocessed_docs)
-
-		index = i.index
-
-		with open(r.resolve_path(environ.get("INDEX_DUMP")), "wb") as output:
-				pickle.dump(i.index, output)
-		
-	else:
-		with open(r.resolve_path(environ.get("INDEX_DUMP")), "rb") as input:
-				index = pickle.load(input)
-
-	q = QueryProcessor(preprocessor=p, index=index)
-
-	app = Flask(__name__)
-
-	@app.route("/", methods=["POST"])
-	def get_docs():
-		if "query" not in request.json:
-			return { "error": "ERROR!" }
-		return jsonify(q.process(request.json["query"], alpha=float(environ.get("THRESHOLD_VALUE"))))
-
-	app.run()
+	app.run(port=5000)
 	
---
+++ index.py
@@ -0,0 +1,47 @@
+from os import environ
+from lib.preprocessor import Preprocessor
+from lib.reader import Reader
+from lib.index_builder import IndexBuilder
+from lib.query_processor import QueryProcessor
+import pickle
+
+if __name__ == '__main__':
+	r = Reader(collection_path=environ.get("COLLECTION_PATH"))
+	stopwords = list(filter(lambda s: s != " ", r.read_file(environ.get("STOPWORDS_PATH")).splitlines()))
+	p = Preprocessor(stopwords=stopwords)
+
+	r.mkdir(environ.get("DUMPS_PATH"))
+
+	preprocessed_docs = None
+
+	if not r.path_exists(environ.get("PREPROCESSED_DUMP")):
+		docs = r.get_documents()
+		abstract = r.read_file(docs[0])
+
+		abstracts = []
+		for doc in docs:
+			abstracts.append(r.read_file(doc))
+
+		preprocessed_docs = p.preprocess_pipe(abstracts)
+	
+		with open(r.resolve_path(environ.get("PREPROCESSED_DUMP")), "wb") as output:
+			pickle.dump(preprocessed_docs, output)
+	else:
+		with open(r.resolve_path(environ.get("PREPROCESSED_DUMP")), "rb") as input:
+			preprocessed_docs = pickle.load(input)
+
+	index = None
+	if not r.path_exists(environ.get("INDEX_DUMP")):
+		i = IndexBuilder()
+		i.process(preprocessed_docs)
+
+		index = i.index
+
+		with open(r.resolve_path(environ.get("INDEX_DUMP")), "wb") as output:
+				pickle.dump(i.index, output)
+		
+	else:
+		with open(r.resolve_path(environ.get("INDEX_DUMP")), "rb") as input:
+				index = pickle.load(input)
+
+	q = QueryProcessor(preprocessor=p, index=index)
